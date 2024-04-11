a16e112f238f932a2cd2afec540a3b405a39dd8b
rozakeropyan <164854118+rozakeropyan@users.noreply.github.com>
2024-04-11 14:43:26 +0400

    Update craps.py

--- craps.py
+++ craps.py
@@ -2,20 +2,20 @@ from random import randint
 
 def main():
     while True:
-        number_1 = randint(0,13)
-        number_2 = randint(0,13)
+        number_1 = randint(1,6)
+        number_2 = randint(1,6)
         sum = number_1 + number_2
         if (sum == 7 or sum == 11):
             print("The sum of dice is {} + {} = {}".format(number_1, number_2, sum))
             print("You Won")
             print("========")
             break
-        elif (sum == 2 or sum == 3 or sum == 12):
+        elif sum in (2,3,12):
             print("The sum of dice is {} + {} = {}".format(number_1, number_2, sum))
             print("Casino wins")
             print("========")
             break
-        elif (sum == 4 or sum == 5 or sum == 6 or sum == 8 or sum == 9 or sum == 10):
+        elif sum in (4,5,6,8,9,10):
             print("The sum of dice is {} + {} = {}".format(number_1, number_2, sum))
             print("Now your goal number is: {}".format(sum))
             while True:
@@ -24,12 +24,10 @@ def main():
                 sum_2 = num1 + num2
                 print("The sum of dice is {} + {} = {}".format(num1, num2, sum_2))
                 if (sum_2 == 7):
-                    print("The sum of dice is {} + {} = {}".format(num1, num2, sum_2))
                     print("You lose")
                     print("========")
                     break
                 elif(sum == sum_2):
-                    print("The sum of dice is {} + {} = {}".format(num1, num2, sum_2))
                     print("You won")
                     print("========")
                     break
